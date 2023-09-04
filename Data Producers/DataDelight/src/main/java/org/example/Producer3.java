ea1d163aa55ff30faf62718d2042bb7eff2ffe4e
MohammadEhsan Akhavanpour <akhavanm@uwindsor.ca>
2023-09-03 22:37:51 -0400

    First version
    Initialize Java Maven Project
    Description: Initialize the Maven project with a pom.xml and the necessary directory structure.
    Commit Message: Initialize Maven project with basic structure
    Add Dependencies to pom.xml
    Description: Add yfinance and Kafka client dependencies to pom.xml.
    Commit Message: Add yfinance and Kafka client dependencies to pom.xml
    Implement Yahoo Finance API Data Fetch
    Description: Implement the code to fetch stock data for GBPUSD=X from Yahoo Finance using the yfinance API.
    Commit Message: Implement Yahoo Finance API data fetch for GBPUSD=X
    Implement Kafka Producer
    Description: Implement Kafka Producer to send stock data to a Kafka topic.
    Commit Message: Implement Kafka Producer for stock data
    Integrate Yahoo Finance and Kafka Producer
    Description: Integrate Yahoo Finance API data fetch and Kafka Producer to send stock data to a Kafka topic.
    Commit Message: Integrate Yahoo Finance data fetch with Kafka Producer
    Add Basic Error Handling
    Description: Add basic error handling for data fetch and Kafka producer.
    Commit Message: Add basic error handling

---
+++ Data Producers/DataDelight/src/main/java/org/example/Producer3.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import com.yahoofinance.Stock;
+import com.yahoofinance.YahooFinance;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Producer3 {
+    public static void main(String[] args) throws IOException {
+        // Configure Kafka producer
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Fetch data from Yahoo Finance API for GBPUSD=X
+        Stock stock = YahooFinance.get("GBPUSD=X");
+
+        // Prepare the message to send to Kafka
+        String key = "GBPUSD=X";
+        String value = String.format("Price: %s, Change: %s", stock.getQuote().getPrice(), stock.getQuote().getChange());
+
+        // Produce the message to a Kafka topic
+        ProducerRecord<String, String> record = new ProducerRecord<>("stock-prices", key, value);
+        producer.send(record, (metadata, exception) -> {
+            if (exception != null) {
+                // Handle exception
+                exception.printStackTrace();
+            } else {
+                System.out.printf("Produced record to topic %s partition %s with offset %s%n", metadata.topic(), metadata.partition(), metadata.offset());
+            }
+        });
+
+        // Close the producer
+        producer.close();
+    }
+}
+
