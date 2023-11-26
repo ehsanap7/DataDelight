ae3ff49a2f3c31057e937f54cbee963511559cb6
MohammadEhsan Akhavanpour <akhavanm@uwindsor.ca>
2023-11-25 22:26:13 -0500

    feat(kafka): Implement data fetching and initial stream processing
    This commit introduces two major components to the DataDelight project:
    1. A Kafka producer that fetches financial data for predefined symbols and publishes it to the 'MarketData' topic.
    2. A Kafka Streams application that processes the incoming data to handle missing values and perform validity checks before publishing to the 'ProcessedMarketData' topic.
    
    The producer utilizes a mock `getDataForSymbol` method to simulate data retrieval, which should be replaced with actual data-fetching logic. The stream processing app includes placeholder methods for 'handleMissingValues' and 'isValid' to be filled in with appropriate data validation and cleaning logic.
    
    Both components are configured to run against a local Kafka setup and need adjustments for production deployment.
    
    Next steps include:
    - Integration with real data sources.
    - Refining data processing logic to meet business requirements.
    - Scaling the Kafka setup for production use.
    
    Related tickets: #12345 (data fetching), #67890 (stream processing)

---
+++ src/main/java/com/datadelight/kafkamicro/KafkaMicroApplication.java
@@ -0,0 +1,13 @@
+package com.datadelight.kafkamicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMicroApplication.class, args);
+    }
+
+}
